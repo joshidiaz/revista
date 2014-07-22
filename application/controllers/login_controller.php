a94c42d606eb4fec87fdd3bbe3ea68147b96baaa
joshidiaz <josue_sic@hotmail.com>
2014-07-22 15:17:00 -0500

    Base del proyecto

---
+++ application/controllers/login_controller.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login_controller extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+	}
+	public function index()
+	{
+		$this->load->view('login_view', NULL, FALSE);
+	}
+
+}
+
+/* End of file login_controller.php */
+/* Location: ./application/controllers/login_controller.php */
---
+++ application/views/login_view.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ingresar</title>
+	<link rel="stylesheet" href="">
+	
+	<!-- css -->
+  	<link rel="stylesheet" type="text/css" href="<?=base_url();?>statics/foundation/css/normalize.css">
+  	<link rel="stylesheet" type="text/css" href="<?=base_url();?>statics/foundation/css/foundation.css">
+  	<link rel="stylesheet" type="text/css" href="<?=base_url();?>statics/css/estilo.css">
+	<!-- JS -->
+	<script src="<?=base_url();?>statics/foundation/js/vendor/modernizr.js"></script>
+	<script src="<?=base_url();?>statics/foundation/js/vendor/jquery.js"></script>
+	<script src="<?=base_url();?>statics/foundation/js/vendor/placeholder.js"></script>
+	<script src="<?=base_url();?>statics/foundation/js/foundation.min.js"></script>
+	<script src="<?=base_url();?>statics/foundation/js/foundation/foundation.alert.js"></script>
+
+</head>
+
+
+
+<body>
+	<div class="row">
+		<nav class="top-bar" data-topbar>
+			<ul class="title-area">
+				<li class="name">
+					<h1><a href="#">My Site</a></h1>
+				</li> 
+				<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
+				<li class="toggle-topbar menu-icon">
+					<a href="#"><span>Menu</span></a>
+				</li>
+			</ul>
+			<section class="top-bar-section"> 
+				<!-- Right Nav Section --> 
+				<ul class="right"> 
+					<li class="active"><a href="#">Right Button Active</a></li> 
+					<li class="has-dropdown"> 
+						<a href="#">Right Button Dropdown</a> 
+						<ul class="dropdown"> 
+							<li><a href="#">First link in dropdown</a></li> 
+						</ul> 
+					</li> 
+				</ul> 
+				<!-- Left Nav Section --> 
+				<ul class="left"> 
+					<li><a href="#">Left Nav Button</a></li> 
+				</ul> 
+			</section> 
+		</nav>
+	</div>
+	<div class="row">
+		<div class="large-5 small-5 small-centered large-centered columns login bordeBlanco">
+			<div class="large-12 large-centered columns">
+					
+			</div>
+			<fieldset class="fondoForm">
+				<div class="row">
+					<?php if (isset($msj)) {
+						echo $msj;
+					} ?>
+				</div>
+				<form action='<?=base_url();?>index.php/login/verificarUsuario' method='post' name='process' accept-charset="utf-8" enctype="multipart/form-data">
+					<div class="row">
+						<div class="large-8 small-10 small-centered large-centered columns">
+						    <input type="text" name="nombreLabel" placeholder="Usuario"  autofocus required>
+						</div>
+					</div>
+					<div class="row">
+						<div class="large-8 small-10 small-centered large-centered columns">
+						    <input type="password" name="contrasenia" placeholder="Contraseña" required>
+						</div>
+					</div>
+					<div class="row">
+						<div class="large-8 small-10 small-centered large-centered columns">
+							<input type="submit" class="button expand [tiny small large]" value="Ingresar">
+						</div>
+					</div>
+					<div class="large-8 small-10 small-centered large-centered columns">
+						<a class="right" href="#">Recuperar contraseña</a>
+					</div>
+				</form>
+			</fieldset>
+		</div>
+	</div>
+
+	<script>
+		$(document).foundation();
+	</script>
+	
+</body>
+</html>
